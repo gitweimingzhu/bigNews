5bb649c2655d01f76b1d7592f249e3fbf68c0b31
weimingzhu <302867245@qq.com>
2020-10-28 21:28:05 +0800

    表单校验完成

---
+++ assets/js/user/user-password.js
@@ -0,0 +1,22 @@
+$(function () {
+  // 表单校验
+  var form = layui.form
+  form.verify({
+    repass: function  (value,item) {
+      var passVal = $('.pass').val()
+      if (passVal !== value) {
+        $('.myForm .pass').val('')
+        $('.myForm .repass').val('')
+        return '两次输入的密码不一致'
+      }
+    },
+
+    //我们既支持上述函数式的方式，也支持下述数组的形式
+    //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+    pass: [/^[\d]{6,12}$/, '密码必须6到12位数字，且不能出现空格'],
+  })
+  // 重置密码
+  $('.myForm').on('submit', function (e) {
+    e.preventDefault()
+  })
+})
