d4a9c57081101b5ca12eab2c2d15a221e1a7b288
weimingzhu <302867245@qq.com>
2020-10-29 19:54:49 +0800

    更新头像之上传头像完成

--- assets/js/user/user-avatar.js
+++ assets/js/user/user-avatar.js
@@ -19,21 +19,42 @@ $(function () {
     // 2.2 弹出选择文件的窗口
     $('#avatar').click()
   })
+
   // 3. 预览待上传的图片
   // 3.1 给文件按钮注册change事件
   $('#avatar').on('change', function () {
-
     // 3.2 获取待上传的图片
-   var avatar = this.files[0]
+    var avatar = this.files[0]
 
-    // 3.3 生成一个链接
+    // 3.3 生成一个图片地址链接
     var imgUrl = URL.createObjectURL(avatar)
 
     // 3.4 显示到img标签内
-    $('#image')
+    /* $('#image')
       .cropper('destroy')      // 销毁旧的裁剪区域
       .attr('src', imgUrl)  // 重新设置图片路径
-      .cropper(options)        // 重新初始化裁剪区域
+      .cropper(options)        // 重新初始化裁剪区域 */
+    $image.cropper('replace', imgUrl)
+  })
 
+  // 4.上传头像
+  $('.btn-sure').on('click', function () {
+    // 生成base64格式的图片链接
+    var dataURL = $image
+      .cropper('getCroppedCanvas', { width: 100, height: 100 })
+      .toDataURL('image/png')
+    $.ajax({
+      type: 'POST',
+      url: '/my/update/avatar',
+      data: { avatar: dataURL },
+      success: function  (info) {
+        var layer = layui.layer
+        layer.msg(info.message)
+        if (info.status === 0) {
+          // 主页面上的头像进行重新渲染
+          parent.window.getUserInfo()
+        }
+      }
+    })
   })
 })
